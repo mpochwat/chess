572f280ecca0f475b9950233b164412bf2c6661c
Martin <martinochwat1@gmail.com>
2015-02-22 00:28:48 -0500

    Added board showing pieces. Also test for moving of pawn.

--- game.rb
+++ game.rb
@@ -26,14 +26,14 @@ class Board
 				  [Square.new('a2'),Square.new('b2'),Square.new('c2'),Square.new('d2'),Square.new('e2'),Square.new('f2'),Square.new('g2'),Square.new('h2')],
 				  [Square.new('a1'),Square.new('b1'),Square.new('c1'),Square.new('d1'),Square.new('e1'),Square.new('f1'),Square.new('g1'),Square.new('h1')],
 				]
-		@pieces = [Pawn.new('w', 'a2'), Pawn.new('w', 'b2'), Pawn.new('w', 'c2'), Pawn.new('w', 'd2'), 
-				     Pawn.new('w', 'e2'), Pawn.new('w', 'f2'), Pawn.new('w', 'g2'), Pawn.new('w', 'h2'),
-					 Rook.new('w', 'a1'), Knight.new('w', 'b1'), Bishop.new('w', 'c1'), Queen.new('w', 'd1'),
-					 King.new('w', 'e1'), Bishop.new('w', 'f1'), Knight.new('w', 'g1'), Rook.new('w', 'h1'),
-					 Pawn.new('b', 'a7'), Pawn.new('b', 'b7'), Pawn.new('b', 'c7'), Pawn.new('b', 'd7'), 
-				     Pawn.new('b', 'e7'), Pawn.new('b', 'f7'), Pawn.new('b', 'g7'), Pawn.new('b', 'h7'),
-					 Rook.new('b', 'a8'), Knight.new('b', 'b8'), Bishop.new('b', 'c8'), King.new('b', 'd8'),
-					 Queen.new('b', 'e8'), Bishop.new('b', 'f8'), Knight.new('b', 'g8'), Rook.new('b', 'h8')]		
+		@pieces = [Piece.new('pawn','w', 'a2'), Piece.new('pawn','w', 'b2'), Piece.new('pawn','w', 'c2'), Piece.new('pawn','w', 'd2'), 
+				   Piece.new('pawn','w', 'e2'), Piece.new('pawn','w', 'f2'), Piece.new('pawn','w', 'g2'), Piece.new('pawn','w', 'h2'),
+				   Piece.new('rook','w', 'a1'), Piece.new('knight','w', 'b1'), Piece.new('bishop','w', 'c1'), Piece.new('queen','w', 'd1'),
+				   Piece.new('king','w', 'e1'), Piece.new('bishop','w', 'f1'), Piece.new('knight','w', 'g1'), Piece.new('rook','w', 'h1'),
+			  	   Piece.new('pawn','b', 'a7'), Piece.new('pawn','b', 'b7'), Piece.new('pawn','b', 'c7'), Piece.new('pawn','b', 'd7'), 
+				   Piece.new('pawn','b', 'e7'), Piece.new('pawn','b', 'f7'), Piece.new('pawn','b', 'g7'), Piece.new('pawn','b', 'h7'),
+				   Piece.new('rook','b', 'a8'), Piece.new('knight','b', 'b8'), Piece.new('bishop','b', 'c8'), Piece.new('king','b', 'd8'),
+				   Piece.new('queen','b', 'e8'), Piece.new('bishop','b', 'f8'), Piece.new('knight','b', 'g8'), Piece.new('rook','b', 'h8')]		
 	end
 
 	def show_row(row)
@@ -54,8 +54,8 @@ class Board
 		@pieces.each do |piece|
 			@board.each do |row|
 				row.each do |square|
-					if square.show_position == piece.show_position
-						square.occupant_new(piece)
+					if square.position == piece.type.position
+						square.occupant_new(piece.type)
 					end
 				end
 			end
@@ -65,101 +65,130 @@ class Board
 end
 
 class Square
+	attr_reader :position
 
 	def initialize(position)
 		@position = position
 		@occupant = nil
 	end
 
-	def show_position
-		@position
+	def occupant_new(piece)
+		@occupant = piece
 	end
 
 	def show
 		@occupant == nil ? (print @position) : (print @occupant)
+	end	
+
+end
+
+class Piece
+	attr_reader :type
+
+	def initialize(type, color, position)
+		case type
+		when "pawn"
+			@type = Pawn.new(color, position)
+		when "rook"
+			@type = Rook.new(color, position)
+		when "knight"
+			@type = Knight.new(color, position)
+		when "bishop"
+			@type = Bishop.new(color, position)
+		when "queen"
+			@type = Queen.new(color, position)
+		when "king"
+			@type = King.new(color, position)
+		else
+			@type = nil
+		end
 	end
 
-	def occupant_new(piece)
-		@occupant = piece
+	def type
+		@type
 	end
 
 end
 
 class Pawn
+	attr_reader :position
 
 	def initialize(color, position)
 		@color = color
 		@position = position
 	end
 
-	def show_position
-		@position
-	end	
+	def position_legal(pos)
+		if @position[1] == "2"
+			if pos[0] == @position[0] && ( pos[1].to_i == (@position[1].to_i + 1) ) || ( pos[1].to_i == (@position[1].to_i + 2) )
+				true
+			else
+				false
+			end
+		else
+			if pos[0] == @position[0] && pos[1].to_i == (@position[1].to_i + 1)
+				true
+			else
+				false
+			end
+		end
+	end
+
+	def move(pos)
+		if position_legal(pos)
+			@position = pos
+		end
+	end
 
 end
 
 class Rook
+	attr_reader :position
 
 	def initialize(color, position)
 		@color = color
 		@position = position
 	end
-
-	def show_position
-		@position
-	end	
 
 end
 
 class Knight
+	attr_reader :position	
 
 	def initialize(color, position)
 		@color = color
 		@position = position
 	end
-
-	def show_position
-		@position
-	end	
 
 end
 
 class Bishop
+	attr_reader :position
 
 	def initialize(color, position)
 		@color = color
 		@position = position
 	end
-
-	def show_position
-		@position
-	end	
 
 end
 
 class Queen
+	attr_reader :position
 
 	def initialize(color, position)
 		@color = color
 		@position = position
-	end
-
-	def show_position
-		@position
 	end
 
 end
 
 class King
+	attr_reader :position
 
 	def initialize(color, position)
 		@color = color
 		@position = position
 	end
-
-	def show_position
-		@position
-	end	
 
 end
 
@@ -174,3 +203,9 @@ victory = false
 game = Game.new('player1', 'player2')
 game.gameboard.place_pieces
 game.gameboard.show_board
+
+new_pawn = Pawn.new('white', 'a2')
+new_pawn.move('a4')
+p new_pawn.position
+
+# Depending on white OR black's turn, may have to change the "view" of the board
